125884fce254407e844272e570ba15e715d4ead9
smjleo <leolsj1023@gmail.com>
2022-07-30 14:53:24 +0900

    feat: load both ledgers

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const csvtojson = require('csvtojson');
+const totalAmount = core.getInput('total-amount');
+const repoToken = core.getInput('repo-token');
+const octokit = github.getOctokit(repoToken);
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function getLedger(branch) {
+    console.log(`getting ledger on branch ${branch}`)
+    try {
+        const octokitRes = await octokit.rest.repos.getContent({
+            owner: 'plamorg',
+            repo: 'ghcoin',
+            path: 'ledger.csv',
+            branch,
+        });
+
+        const res = await fetch(octokitRes.download_url);
+        const ledgerCsv = await res.text();
+        const ledgerJsonRaw = await csv().fromString(ledgerCsv);
+
+        let ledgerJson = {};
+        for (let i of ledgerJsonRaw) {
+            if (ledgerJson[i.name]) throw `Duplicate user: ${i.name} ${i.balance} ${ledgerJson[i.name]}`;
+            if (/^\d+$/.test(i.balance)) throw `Invalid balance: ${i.name} ${i.balance}`
+            ledgerJson[i.name] = parseInt(i.balance);
+        }
+
+        console.log(`branch ${branch}:`, ledgerJson);
+
+        return ledgerJson;
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+async function run() {
+    try {
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = github.context.payload;
+
+        // get the original ledger
+        const originalLedger = getLedger('master');
+
+        // get the new ledger
+        const newLedger = getLedger(payload.pull_request.head.ref);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
+}
+
+run();
